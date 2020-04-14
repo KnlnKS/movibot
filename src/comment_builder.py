64336b817a0432ccb5b0b088ee3690d703c911db
KnlnKS <soulkks@hotmail.ca>
2020-04-14 19:32:57 -0400

    Added Try/Except
    Should no longer crash when user requests for unreleased movies.

--- src/comment_builder.py
+++ src/comment_builder.py
@@ -4,35 +4,63 @@ base_url = "https://www.imdb.com/title/tt"
 
 
 def search_movie(title):
-    movies = IMDb().search_movie(title)
-    return IMDb().get_movie(movies[0].movieID)
+    try:
+        movies = IMDb().search_movie(title)
+        return IMDb().get_movie(movies[0].movieID)
+    except:
+        return "Error, title cannot be found."
 
 
 def build_comment(movie):
-    line_1 = "##__[" + movie["title"] + " (" + str(movie['year']) + ")](" + base_url + str(movie.getID()) + "/)__"
-    line_2 = "#####Directed by: " + director_to_string(movie)
-    line_3 = "^(Rating: " + str(movie["rating"]) + "    | Genres: " + genre_to_string(movie) + "| Runtime: " + str(
-        movie["runtime"][0]) + " min)"
-    return line_1+"\n"+line_2+"\n"+line_3
+    line_1 = "##__[" + movie["title"] + " (" + get_year(movie) + ")](" + base_url + str(movie.getID()) + "/)__"
+    line_2 = "#####Directed by: " + get_director(movie)
+    line_3 = "^(Rating: " + get_rating(movie) + "    | Genres: " + get_genre(movie) + "| Runtime: " + get_runtime(movie) \
+             + " min)"
+    return line_1 + "\n" + line_2 + "\n" + line_3
 
 
+def get_runtime(movie):
+    try:
+        return str(movie["runtime"][0])
+    except:
+        return "N/A"
 
 
-def director_to_string(movie):
-    temp = ""
-    for x in movie["director"]:
-        if temp == "":
-            temp += x["name"]
-        else:
-            temp += ", " + x["name"]
-    return temp
+def get_rating(movie):
+    try:
+        return str(movie['rating'])
+    except:
+        return "N/A"
 
 
-def genre_to_string(movie):
-    temp = ""
-    for x in movie["genres"]:
-        if temp == "":
-            temp += x
-        else:
-            temp += ", " + x
-    return temp
+def get_year(movie):
+    try:
+        return str(movie['year'])
+    except:
+        return "TBD"
+
+
+def get_director(movie):
+    try:
+        temp = ""
+        for x in movie["director"]:
+            if temp == "":
+                temp += x["name"]
+            else:
+                temp += ", " + x["name"]
+        return temp
+    except:
+        return "Unknown"
+
+
+def get_genre(movie):
+    try:
+        temp = ""
+        for x in movie["genres"]:
+            if temp == "":
+                temp += x
+            else:
+                temp += ", " + x
+        return temp
+    except:
+        return "Unknown"
--- src/main.py
+++ src/main.py
@@ -4,17 +4,21 @@ import re
 import src.comment_builder
 import src.comment_scrapper
 
-
-r=praw.Reddit("movibot")
-subreddit=r.subreddit("pythonforengineers")
+r = praw.Reddit("movibot")
+subreddit = r.subreddit("pythonforengineers")
 
 for comment in subreddit.stream.comments():
     if re.search("!movibot", comment.body, re.IGNORECASE):
-            title=src.comment_scrapper.search_comment_for_title(comment.body)
-            movie = src.comment_builder.search_movie(title)
-            print(movie)
+        title = src.comment_scrapper.search_comment_for_title(comment.body)
+        movie = src.comment_builder.search_movie(title)
+        if movie != "Error, title cannot be found.":
             reply = src.comment_builder.build_comment(movie)
             comment.reply(reply)
             print("Commented: ")
             print(reply)
+            print("--------------------------------------------------------")
+        else:
+            comment.reply(movie)
+            print("Commented: ")
+            print("Error, title cannot be found.")
             print("--------------------------------------------------------")
